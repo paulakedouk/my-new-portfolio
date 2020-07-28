efa83f5896531bccef5c20d60ebf74d943e1cd02
Paula Kedouk <paulakedouk@gmail.com>
2020-07-27 22:16:13 -0700

    Adjust greensock

--- public/assets/app.js
+++ public/assets/app.js
@@ -5,7 +5,7 @@ let self = this;
 
 fadeElem.forEach(function(self) {
   // build a tween
-  let tween = TweenMax.from(self, 0.5, {autoAlpha: 0, y: '+=70', ease:Linear.easeNone});
+  let tween = TweenMax.from(self, 0.5, {autoAlpha: 0, y: '+=100', ease: Linear.easeNone});
   // build a scene
   let scene = new ScrollMagic.Scene({
     triggerElement: self
@@ -13,7 +13,6 @@ fadeElem.forEach(function(self) {
   .setTween(tween)
   .addTo(controller)
 });
-
   
 function hideAndShow() {
     var hideProjects = document.getElementById('projects-hidden');
@@ -37,40 +36,38 @@ function hireMeScroll() {
 }
 
 
-var mediaQueryMedium = window.matchMedia("(max-width: 768px)");
-
-
-if(!mediaQueryMedium.matches) {
-  $(document).mousemove(function(event){
-    var xPos = (event.clientX/$(window).width())-0.5,
-        yPos = (event.clientY/$(window).height())-0.5,
-        box = $('.contact-img');
+// var mediaQueryMedium = window.matchMedia("(max-width: 768px)");
+// if(!mediaQueryMedium.matches) {
+//   $(document).mousemove(function(event){
+//     var xPos = (event.clientX/$(window).width())-0.5,
+//         yPos = (event.clientY/$(window).height())-0.5,
+//         box = $('.contact-img');
    
-    TweenLite.to(box, 0.6, {
-      rotationY: 60 * xPos, 
-      rotationX: 60 * yPos,
-      ease: Power1.easeOut,
-      transformPerspective: 900,
-      transformOrigin: 'center'
-    });
-  });
-}
+//     TweenLite.to(box, 0.6, {
+//       rotationY: 60 * xPos, 
+//       rotationX: 60 * yPos,
+//       ease: Power1.easeOut,
+//       transformPerspective: 900,
+//       transformOrigin: 'center'
+//     });
+//   });
+// }
 
 
-var submitButton = document.getElementsByClassName("submit-button");
-(submitButton),
-    $(function () {
-        var e = $("#my-form");
-        $(e).submit(function (t) {
-            if ((t.preventDefault(), "" !== $("#name").val() && "" !== $("#email").val() && "" !== $("#message").val())) {
-                $("#submit").attr("value", "Sending...");
-                var n = $(e).serialize();
-                $.ajax({ type: "POST", url: $(e).attr("action"), data: n }).done(function (e) {
-                    var t = document.getElementById("submit-message");
-                    t.style.display = "block";
-                    var n = document.getElementById("submit-button");
-                    (n.style.display = "none"), $("#name").val(""), $("#email").val(""), $("#message").val("");
-                });
-            }
-        });
-    });
+// var submitButton = document.getElementsByClassName("submit-button");
+// (submitButton),
+//     $(function () {
+//         var e = $("#my-form");
+//         $(e).submit(function (t) {
+//             if ((t.preventDefault(), "" !== $("#name").val() && "" !== $("#email").val() && "" !== $("#message").val())) {
+//                 $("#submit").attr("value", "Sending...");
+//                 var n = $(e).serialize();
+//                 $.ajax({ type: "POST", url: $(e).attr("action"), data: n }).done(function (e) {
+//                     var t = document.getElementById("submit-message");
+//                     t.style.display = "block";
+//                     var n = document.getElementById("submit-button");
+//                     (n.style.display = "none"), $("#name").val(""), $("#email").val(""), $("#message").val("");
+//                 });
+//             }
+//         });
+//     });
